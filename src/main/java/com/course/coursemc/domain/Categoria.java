170c4c4fa3efb5be69c82e8c4c03712e911d8d0a
Gabriel Cardoso de Souza <gabrielcardososouza3007@gmail.com>
2022-01-12 23:36:36 -0300

    Banco de Dados H2 e criação automatica da base de dados

--- src/main/java/com/course/coursemc/domain/Categoria.java
+++ src/main/java/com/course/coursemc/domain/Categoria.java
@@ -1,11 +1,24 @@
 package com.course.coursemc.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+
 public class Categoria implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+
+    public Categoria(){
+    }
 
     public Categoria(Integer id, String name) {
         super();
