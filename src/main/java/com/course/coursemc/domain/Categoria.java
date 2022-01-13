11fd6d7413b56fcb35cb234dc50ab9916b8b3c73
Gabriel Cardoso de Souza <gabrielcardososouza3007@gmail.com>
2022-01-12 23:06:14 -0300

    Testando a primeira classe de domínio - Categoria

---
+++ src/main/java/com/course/coursemc/domain/Categoria.java
@@ -0,0 +1,44 @@
+package com.course.coursemc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+    private Integer id;
+    private String name;
+
+    public Categoria(Integer id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria)) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- src/main/java/com/course/coursemc/resources/CategoriaResource.java
+++ src/main/java/com/course/coursemc/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.course.coursemc.resources;
 
+import com.course.coursemc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "Rest esta funcionando!";
-    }
+    public List<Categoria> listar() {
 
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> list = new ArrayList<>();
+        list.add(cat1);
+        list.add(cat2);
+
+        return list;
+
+    }
 }
