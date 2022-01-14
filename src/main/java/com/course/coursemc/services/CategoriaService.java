b52802a007584362abffcf1fdd798a63e18f12cb
Gabriel Cardoso de Souza <gabrielcardososouza3007@gmail.com>
2022-01-14 19:52:55 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/course/coursemc/services/CategoriaService.java
+++ src/main/java/com/course/coursemc/services/CategoriaService.java
@@ -20,5 +20,8 @@ public class CategoriaService   {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado. ID: "
                 + id + ", tipo: " + Categoria.class.getName()));
     }
-
+    public Categoria insert(Categoria obj){
+        obj.setId(null);
+        return repo.save(obj);
+    }
 }
